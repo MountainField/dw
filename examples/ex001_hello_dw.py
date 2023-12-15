5d230092b3dad56d313c1d2c03197b35ab009ff2
nogayama <NOGAYAMA@jp.ibm.com>
2023-12-15 09:07:48 +0900

    feat: add python package structure
    Signed-off-by: nogayama <NOGAYAMA@jp.ibm.com>

---
+++ examples/ex001_hello_dw.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import dw
+
+print(dw.DEFAULT_ENCODING)
---
+++ src/dw/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# =================================================================
+# dw
+#
+# Copyright (c) 2022 Takahide Nogayama
+#
+# This software is released under the MIT License.
+# http://opensource.org/licenses/mit-license.php
+# =================================================================
+
+__version__ = "0.6.0"
+
+import logging as _logging
+import sys as _sys
+
+# Logger
+_LOGGER: _logging.Logger = _logging.getLogger(__name__)
+_LOG_FORMAT: str = '%(asctime)s |  %(levelname)-7s | %(message)s (%(filename)s L%(lineno)s %(name)s)'
+
+DEFAULT_ENCODING = "utf-8"
+
+if __name__ == "__main__":
+    _logging.basicConfig(stream=_sys.stderr, format=_LOG_FORMAT, level=_logging.WARNING)
